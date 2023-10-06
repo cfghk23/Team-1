cac0c87e42a582b60482973bebb947ceb68ffc65
Patrick Wenas <patrick@nefertiti.tech>
2023-10-06 12:02:29 +0800

    courses add

--- backend_2/routes/courses_route.py
+++ backend_2/routes/courses_route.py
@@ -1,10 +1,14 @@
 from fastapi import APIRouter, Body, Depends, HTTPException
 from database import database
 
-users_collection = database.get_collection('student')
+users_collection = database.get_collection('courses')
 router = APIRouter()
 
 @router.get("/", response_description="Get all artists")
-async def get_artists(name: str = '')  -> list:
-    user = await users_collection.find_one()
-    return user
+async def insert_course(name: str = '')  -> list:
+    data = {
+        'name': 'hi',
+        'class': 'd'
+    }
+    user = await users_collection.insert_one(data)
+    return ''
