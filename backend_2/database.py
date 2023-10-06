8931e04ac8312559f5e3f0331a514895b7b497df
Patrick Wenas <88708679+pat8379@users.noreply.github.com>
2023-10-06 22:02:22 +0800

    Update database.py

--- backend_2/database.py
+++ backend_2/database.py
@@ -2,7 +2,7 @@ import motor.motor_asyncio
 from decouple import config
 
 #DB Connection Objects and Variables
-MONGO_URL = 'mongodb+srv://pats:arcticmonkeys@firstever.nrokl.mongodb.net/test'
+MONGO_URL = ''
 MONGO_DB = 'cfg'
 
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URL)
