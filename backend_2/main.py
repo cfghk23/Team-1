d41471e3d04b0bca288cfd31b7f217d489cc0e19
Patrick Wenas <patrick@nefertiti.tech>
2023-10-06 11:17:42 +0800

    set up

---
+++ backend_2/main.py
@@ -0,0 +1,56 @@
+from fastapi import FastAPI
+import uvicorn
+# from routes.user_route import router as UserRouter
+# from routes.product_route import router as ProductRouter
+# from routes.portfolio_route import router as PortfolioRouter
+# from routes.leaderboard_route import router as LeaderboardRouter
+from fastapi.middleware.cors import CORSMiddleware
+# from scheduler.alert_scheduler import AlertsScheduler
+import logging
+from decouple import config
+# from database import database
+
+
+DOCS_ENABLED = config("docs") == "True"
+
+logging.basicConfig(level=logging.INFO)
+
+
+if __name__ == "__main__":
+    uvicorn.run("server.app:app", host="127.0.0.1",
+                port=8001, reload=True, log_level="info")
+
+# alerts_scheduler = AlertsScheduler()
+# alerts_scheduler.run(60)
+
+app = FastAPI(docs_url="/docs" if DOCS_ENABLED else None,
+              redoc_url="/redoc" if DOCS_ENABLED else None)
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# app.include_router(UserRouter, tags=["Users"], prefix="/v0.1/users")
+# app.include_router(ProductRouter, tags=["Products"], prefix="/v0.1/products")
+# app.include_router(PortfolioRouter, tags=[
+#                    "Portfolios"], prefix="/v0.1/portfolios")
+# app.include_router(LeaderboardRouter, tags=[
+#                    "Leaderboards"], prefix="/v0.1/leaderboards")
+
+
+@app.get("/")
+async def root():
+    return {"message": "Pong"}
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8001)
