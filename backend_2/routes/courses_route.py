fe859fbcb80cf9a2267491ad0faa66988db81347
Patrick Wenas <patrick@nefertiti.tech>
2023-10-06 12:36:54 +0800

    courses route

--- backend_2/routes/courses_route.py
+++ backend_2/routes/courses_route.py
@@ -4,11 +4,24 @@ from database import database
 users_collection = database.get_collection('courses')
 router = APIRouter()
 
-@router.get("/", response_description="Get all artists")
-async def insert_course(name: str = '')  -> list:
+@router.get("/", response_description="Get all courses")
+async def get_all_course() -> list:
+    users = users_collection.find({}, {'_id': 0})
+    items = [item async for item in users]
+    return items
+
+@router.get("/{name}",)
+async def get_all_course(name) -> list:
+    users = users_collection.find({'name': name}, {'_id': 0})
+    items = [item async for item in users]
+    return items
+
+@router.post("/insert", response_description="insert course")
+async def insert_course(body: dict = Body(...)):
     data = {
-        'name': 'hi',
-        'class': 'd'
+        'name': body['name'],
+        'class': body['class'],
+        'questions': body['questions'],
     }
     user = await users_collection.insert_one(data)
-    return ''
+    return 'class added'
